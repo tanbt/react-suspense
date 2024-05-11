f709450e20c6214e083c0d82adf97e39138cd99a
tan.bui@virta.global <tan.bui@virta.global>
2024-05-11 16:45:14 +0300

    Wrap with error boundary

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,34 +1,30 @@
 // Simple Data-fetching
 // http://localhost:3000/isolated/exercise/01.js
-
+import {ErrorBoundary} from 'react-error-boundary'
 import * as React from 'react'
-// 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
 import {PokemonDataView, fetchPokemon} from '../pokemon'
 
-// 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
-
-// 🐨 create a variable called "pokemon" (using let)
 let pokemon
-// 💣 delete this now...
-// const pokemon = {
-//   name: 'TODO',
-//   number: 'TODO',
-//   attacks: {
-//     special: [{name: 'TODO', type: 'TODO', damage: 'TODO'}],
-//   },
-//   fetchedAt: 'TODO',
-// }
+let pokemonError
 
 // We don't need the app to be mounted to know that we want to fetch the pokemon
 // named "pikachu" so we can go ahead and do that right here.
 // 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
 // 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
 // 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
-const pokemonPromise = fetchPokemon('pikachu').then(pokemonData => {
-  pokemon = pokemonData
-})
+const pokemonPromise = fetchPokemon('pikachu') // try a wrong name to see the error boundary
+  .then(pokemonData => {
+    pokemon = pokemonData
+  })
+  .catch(error => {
+    pokemonError = error
+  })
 
 function PokemonInfo() {
+  if (pokemonError) {
+    throw pokemonError
+  }
+
   // 🐨 if there's no pokemon yet, then throw the pokemonPromise
   // 💰 (no, for real. Like: `throw pokemonPromise`)
   if (!pokemon) {
@@ -52,10 +48,12 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        {/* 🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback */}
-        <React.Suspense fallback={<div>Loading animal...</div>}>
-          <PokemonInfo />
-        </React.Suspense>
+        <ErrorBoundary fallback={<div>Failed to load Pokemon</div>}>
+          {/* 🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback */}
+          <React.Suspense fallback={<div>Loading animal...</div>}>
+            <PokemonInfo />
+          </React.Suspense>
+        </ErrorBoundary>
       </div>
     </div>
   )
